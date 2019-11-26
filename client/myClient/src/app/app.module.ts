dd89965fbc4258a3bc3af08fc154478a35e7d1eb
lwx <lwx@lwxs-MacBook-Air.local>
2019-11-26 11:26:10 +0800

    changed to using model binding for form

--- client/myClient/src/app/app.module.ts
+++ client/myClient/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { CategorySearchComponent } from './category-search/category-search.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SearchFormComponent } from './search-form/search-form.component';
+
+const appRoutes: Routes = [
+  { path: 'something', component: CategorySearchComponent, pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    CategorySearchComponent,
+    SearchFormComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
